427c66b79c28a1491f35422c1cf2d3480118ed0d
Nichole Huffman <nicholeh@users.noreply.github.com>
2021-09-06 11:03:52 -0500

    [11] Style tweak for large monitors NH

--- story-grimoire/src/components/view/SinglePageWithInnerNav/index.jsx
+++ story-grimoire/src/components/view/SinglePageWithInnerNav/index.jsx
@@ -4,7 +4,7 @@ import Grid from '../../structure/Grid'
 import InPageNavigation from '../../structure/InPageNavigation'
 
 const SinglePageWithInnerNav = ({ navLinks, children }) => (
-    <Grid className="lg:grid-cols-5 lg:gap-10">
+    <Grid className="lg:grid-cols-5 xxxl:grid-cols-6 lg:gap-10">
         <InPageNavigation className="col-span-1" links={navLinks} />
         <div className="col-span-1 lg:col-span-4">{children}</div>
     </Grid>
